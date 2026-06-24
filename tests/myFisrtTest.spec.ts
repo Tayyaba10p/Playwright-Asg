64a9e856de0af5925cc5a64b6ba7b6396439848d
Tayyaba Ellahi <tayyaba.ellahi@10pearls.com>
2026-06-24 15:35:43 +0500

    Pipeline1

--- tests/myFisrtTest.spec.ts
+++ tests/myFisrtTest.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/tests';
+import { test, expect } from '@playwright/test';
 
 test('Verify user can open Playwright website', async ({ page }) => {
   // Navigate to website
